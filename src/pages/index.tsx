0d4f11910226fe668e3c67384b8a77578cb5f758
Marcon Willian <oi@marconwillian.dev>
2021-04-05 07:16:49 -0300

    chore ✨: Create login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,62 @@
+import { Flex, Input, FormLabel, FormControl, Button, Stack } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-    <h1>Dash</h1>
+    <Flex 
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex 
+        as="form" 
+        width="100%" 
+        maxWidth={360}
+        background="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              backgroundColor="gray.900"
+              variant="filled"
+              _hover={{
+                backgroundColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input 
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              backgroundColor="gray.900"
+              variant="filled"
+              _hover={{
+                backgroundColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button 
+          type="submit"
+          mt="8"
+          colorScheme="pink" 
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
