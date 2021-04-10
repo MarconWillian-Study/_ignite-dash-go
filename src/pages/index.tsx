5d00f5ea86cd9e217624e4d2791129da12b9bbff
Marcon Willian <oi@marconwillian.dev>
2021-04-10 17:02:53 -0300

    chore ✨: Add validation of signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,46 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
+import { useRouter } from 'next/dist/client/router';
 import Link from 'next/link';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { Input } from '../components/Form/Input'
 
+interface SignInFormData {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = Yup.object().shape({
+  email: Yup.string().required('E-mail obrigatório').email('E=mail inválido'),
+  password: Yup.string().required('Senha obrigatória')
+})
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+  const router = useRouter();
+
+
+  const { errors } = formState;
+
+  console.log(errors);
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(values)
+    router.push('/dashboard');    
+  }
+
   return (
     <Flex 
-      width="100vw"
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
+    width="100vw"
+    height="100vh"
+    alignItems="center"
+    justifyContent="center"
     >
       <Flex 
         as="form" 
@@ -19,30 +50,42 @@ export default function SignIn() {
         padding="8"
         borderRadius={8}
         flexDirection="column"
-      >
+        onSubmit={handleSubmit(handleSignIn)}
+        >
         <Stack spacing="4">
           <Input
             name="email"
             type="email"
             label="Email"
-          />
+            error={errors.email}
+            {
+              ...register(
+                'email'
+              )
+            }
+            />
           <Input
             name="password"
             type="password"
             label="Senha"
+            error={errors.password}
+            {
+              ...register(
+                'password'
+              )
+            }
           />
         </Stack>
 
-        <Link href="/dashboard" passHref>
-          <Button 
-            type="submit"
-            mt="8"
-            colorScheme="pink" 
-            size="lg"
-          >
-            Entrar
-          </Button>
-        </Link>
+        <Button 
+          type="submit"
+          mt="8"
+          colorScheme="pink" 
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
       </Flex>
     </Flex>
   )
