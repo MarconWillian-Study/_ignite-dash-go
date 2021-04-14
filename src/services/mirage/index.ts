070737912d805110624b551f3474d03e87cffa09
Marcon Willian <oi@marconwillian.dev>
2021-04-14 08:11:32 -0300

    chore ✨: Create controller for mirage pagination

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,10 +1,10 @@
-import { createServer, Factory, Model } from 'miragejs'
+import { createServer, Factory, Model, Response } from 'miragejs'
 import faker from 'faker';
 
 type User = {
   name: string;
   email: string;
-  created_at: string;
+  createdAt: string;
 }
 
 
@@ -29,14 +29,31 @@ export function makeServer(){
     },
 
     seeds(server){
-      server.createList('user', 10);
+      server.createList('user', 200);
     },
 
     routes(){
       this.namespace = 'api';
       this.timing
 
-      this.get('/users');
+      this.get('/users', (schema, request) => {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = schema.all('user').models.slice(pageStart, pageEnd);
+
+        return new Response(
+          200, 
+          {
+            "x-total-count": String(total)
+          },
+          users
+        );
+      });
       this.post('/users');
 
       this.namespace = '';
